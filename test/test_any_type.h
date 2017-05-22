d4cebeddb2dbc0e15f62cb11bff6699acf7e112e
mouyun1115 <563719171@qq.com>
2017-05-22 10:56:06 +0800

    remove any_type test and ioc test

--- test/test_any_type.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TEST_ANY_TYPE_H_BY_MOUYUN_2017_05_18__
-#define __TEST_ANY_TYPE_H_BY_MOUYUN_2017_05_18__
-
-#include "../my_any_type.hpp"
-
-
-
-#endif
--- test/test_ioc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __TEST_IOC_H_BY_MOUYUN_2017_05_18__
-#define __TEST_IOC_H_BY_MOUYUN_2017_05_18__
-
-#include "../my_ioc.hpp"
-
-
-
-#endif
